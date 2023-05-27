91de4b585889ed921894579f8ff32a9cc7ff96bd
emirhantulimat <126756059+emirhantulimat@users.noreply.github.com>
2023-05-27 22:09:05 +0300

    Added migrations

---
+++ Migrations/20230527190534_InitialDBCreation.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MetaX.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDBCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategoriesTable",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoriesTable", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventsTable",
+                columns: table => new
+                {
+                    EventID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventsTable", x => x.EventID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersTable",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersTable", x => x.UserID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReservationsTable",
+                columns: table => new
+                {
+                    ReservationID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventID = table.Column<int>(type: "int", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    ReservationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReservationsTable", x => x.ReservationID);
+                    table.ForeignKey(
+                        name: "FK_ReservationsTable_EventsTable_EventID",
+                        column: x => x.EventID,
+                        principalTable: "EventsTable",
+                        principalColumn: "EventID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ReservationsTable_UsersTable_UserID",
+                        column: x => x.UserID,
+                        principalTable: "UsersTable",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReviewsTable",
+                columns: table => new
+                {
+                    ReviewID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventID = table.Column<int>(type: "int", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReviewsTable", x => x.ReviewID);
+                    table.ForeignKey(
+                        name: "FK_ReviewsTable_EventsTable_EventID",
+                        column: x => x.EventID,
+                        principalTable: "EventsTable",
+                        principalColumn: "EventID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ReviewsTable_UsersTable_UserID",
+                        column: x => x.UserID,
+                        principalTable: "UsersTable",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservationsTable_EventID",
+                table: "ReservationsTable",
+                column: "EventID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservationsTable_UserID",
+                table: "ReservationsTable",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReviewsTable_EventID",
+                table: "ReviewsTable",
+                column: "EventID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReviewsTable_UserID",
+                table: "ReviewsTable",
+                column: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoriesTable");
+
+            migrationBuilder.DropTable(
+                name: "ReservationsTable");
+
+            migrationBuilder.DropTable(
+                name: "ReviewsTable");
+
+            migrationBuilder.DropTable(
+                name: "EventsTable");
+
+            migrationBuilder.DropTable(
+                name: "UsersTable");
+        }
+    }
+}
