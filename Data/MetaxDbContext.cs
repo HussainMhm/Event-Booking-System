696c7c32f2aeae177b275cffdf562090c61a7a6d
hussain <hussainabdullatif@marun.edu.tr>
2023-05-27 20:44:53 +0300

    Added Entity Framework

---
+++ Data/MetaxDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MetaX.Data
+{
+	public class MetaxDbContext: DbContext
+	{
+		public MetaxDbContext(DbContextOptions options) : base(options)
+		{ }
+	}
+
+	//public DbSet<> MyProperty { get; set; }
+}
+
