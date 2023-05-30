f34ffa8d35015a49d35046cb4bc36cbec8e74551
emirhantulimat <126756059+emirhantulimat@users.noreply.github.com>
2023-05-30 16:23:41 +0300

    Added libraries

--- Pages/Admin/Reservation/Index.cshtml.cs
+++ Pages/Admin/Reservation/Index.cshtml.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection.Metadata;
 
 namespace MetaX.Pages.Admin.Reservation
 {
