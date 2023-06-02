9b3df10a3b2dd7203b5454e416272bf8f46d0bd1
hussain <hussainabdullatif@marun.edu.tr>
2023-06-02 23:35:17 +0300

    draft files
    will be deleted soon

--- Pages/Admin/Event/Delete.cshtml.cs
+++ Pages/Admin/Event/Delete.cshtml.cs
@@ -1,16 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using MetaX.Data;
+using MetaX.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MetaX.Pages.Admin.Event
 {
-	public class DeleteModel : PageModel
+    public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly MetaxDbContext _db;
+
+        [BindProperty]
+        public Model.Event Event { get; set; }
+
+        public DeleteModel(MetaxDbContext db)
         {
+            _db = db;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Event = _db.EventsTable.Find(id);
+            if (Event == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost(int id)
+        {
+            var eventToDelete = _db.EventsTable.Find(id);
+            if (eventToDelete == null)
+            {
+                return NotFound();
+            }
+
+            _db.EventsTable.Remove(eventToDelete);
+            _db.SaveChanges();
+
+            return RedirectToPage("./Index");
         }
     }
 }
