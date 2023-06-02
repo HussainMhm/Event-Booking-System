8146353e58a620fdfd5e11825f4b69b167016d14
hussain <hussainabdullatif@marun.edu.tr>
2023-06-02 18:34:30 +0300

    Added the fetch category function from the db

--- Pages/Admin/Category/Index.cshtml.cs
+++ Pages/Admin/Category/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using MetaX.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,18 @@ namespace MetaX.Pages.Admin.Category
 {
     public class IndexModel : PageModel
     {
+        private readonly MetaxDbContext _db;
+
+        public List<Model.Category> CategoryListing;
+
+        public IndexModel(MetaxDbContext db)
+        {
+           _db = db;
+        }
+
         public void OnGet()
         {
+            CategoryListing = _db.CategoriesTable.ToList();
         }
     }
 }
