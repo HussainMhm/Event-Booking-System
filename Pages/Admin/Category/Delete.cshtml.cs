7e103e9a76a3b73ce2f4857defca9f949ace458e
hussain <hussainabdullatif@marun.edu.tr>
2023-06-02 19:28:03 +0300

    Added initial approach for the delete function

--- Pages/Admin/Category/Delete.cshtml.cs
+++ Pages/Admin/Category/Delete.cshtml.cs
@@ -2,15 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MetaX.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MetaX.Pages.Admin.Category
 {
-	public class DeleteModel : PageModel
+    [Area("Admin")]
+    public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly MetaxDbContext _db;
+
+        public DeleteModel(MetaxDbContext db)
         {
+            _db = db;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public IActionResult OnGet()
+        {
+            var category = _db.CategoriesTable.Find(CategoryId);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            CategoryName = category.Name;
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            var category = _db.CategoriesTable.Find(CategoryId);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _db.CategoriesTable.Remove(category);
+            _db.SaveChanges();
+
+            return RedirectToPage("./Index");
         }
     }
 }
