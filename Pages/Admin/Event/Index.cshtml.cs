d5a2003e0fcaba19a374f08baa9d636141965e34
emirhantulimat <126756059+emirhantulimat@users.noreply.github.com>
2023-06-03 00:23:41 +0300

    Added the delete function

--- Pages/Admin/Event/Index.cshtml.cs
+++ Pages/Admin/Event/Index.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using MetaX.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 
@@ -18,6 +19,16 @@ namespace MetaX.Pages.Admin.Event
         public void OnGet()
         {
             EventListing = _db.EventsTable.ToList();
+        }
+        public IActionResult OnPostDelete(int eventId)
+        {
+            var _event = _db.EventsTable.Find(eventId);
+            if (_event != null)
+            {
+                _db.EventsTable.Remove(_event);
+                _db.SaveChanges();
+            }
+            return RedirectToPage();
         }
     }
 }
