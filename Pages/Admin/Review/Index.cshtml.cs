4da90c5c919e08ab7eae8552af28520581c92850
emirhantulimat <126756059+emirhantulimat@users.noreply.github.com>
2023-06-02 22:28:37 +0300

    Added the get function from the db

--- Pages/Admin/Review/Index.cshtml.cs
+++ Pages/Admin/Review/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using MetaX.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,18 @@ namespace MetaX.Pages.Admin.Review
 {
     public class IndexModel : PageModel
     {
+        private readonly MetaxDbContext _db;
+
+        public List<Model.Review> ReviewListing;
+
+        public IndexModel(MetaxDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            ReviewListing = _db.ReviewsTable.ToList();
         }
     }
 }
