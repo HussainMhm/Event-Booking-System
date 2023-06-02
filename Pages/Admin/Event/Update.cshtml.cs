2a029f8a53155a9c1b8684ea0d8783772881a22f
hussain <hussainabdullatif@marun.edu.tr>
2023-06-02 18:33:36 +0300

    Changed file name

--- Pages/Admin/Event/Update.cshtml.cs
+++ Pages/Admin/Event/Update.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MetaX.Pages.Admin
+namespace MetaX.Pages.Admin.Event
 {
-	public class EventEditModel : PageModel
+	public class UpdateModel : PageModel
     {
         public void OnGet()
         {
