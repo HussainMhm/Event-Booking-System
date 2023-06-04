09f3b721185fde8a338b71bbbf883555f178f75c
hussain <hussainabdullatif@marun.edu.tr>
2023-06-04 20:34:01 +0300

    Connected the events page with db

--- Pages/Events.cshtml.cs
+++ Pages/Events.cshtml.cs
@@ -2,15 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using MetaX.Model;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace MetaX.Pages
 {
-	public class EventsModel : PageModel
+    public class EventsModel : PageModel
     {
-        public void OnGet()
+        private readonly MetaX.Data.MetaxDbContext _context;
+
+        public EventsModel(MetaX.Data.MetaxDbContext context)
         {
+            _context = context;
+        }
+
+        public IList<Model.Event> Events { get; set; }
+
+        public IList<Category> Categories { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public async Task OnGetAsync(int? pageNumber, string category)
+        {
+            var pageSize = 9;
+
+            IQueryable<Model.Event> eventsQuery = _context.EventsTable.AsQueryable();
+
+            // Fetch categories from the database
+            Categories = await _context.CategoriesTable.ToListAsync();
+
+            // Filter events by category if a category is selected
+            if (!string.IsNullOrEmpty(category))
+            {
+                eventsQuery = eventsQuery.Where(e => e.Category == category);
+            }
+
+            // Calculate total pages
+            var count = await eventsQuery.CountAsync();
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            // Paginate events
+            if (pageNumber.HasValue && pageNumber.Value > 1)
+            {
+                eventsQuery = eventsQuery.Skip((pageNumber.Value - 1) * pageSize);
+            }
+
+            eventsQuery = eventsQuery.Take(pageSize);
+
+            Events = await eventsQuery.ToListAsync();
         }
     }
 }
