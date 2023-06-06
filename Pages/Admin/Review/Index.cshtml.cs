c630b4f33f12fd16e36939f017512830413ecd54
hussain <hussainabdullatif@marun.edu.tr>
2023-06-06 20:52:07 +0300

    Changed the way reviews are fetched

--- Pages/Admin/Review/Index.cshtml.cs
+++ Pages/Admin/Review/Index.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using MetaX.Data;
+using MetaX.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -11,27 +12,45 @@ namespace MetaX.Pages.Admin.Review
     {
         private readonly MetaxDbContext _db;
 
-        public List<Model.Review> ReviewListing;
+        public List<ReviewDetails> Reviews { get; set; }
 
         public IndexModel(MetaxDbContext db)
         {
             _db = db;
         }
 
-        public void OnGet(string eventName, int? rating)
+        public async Task OnGetAsync()
         {
-            IQueryable<Model.Review> query = _db.ReviewsTable
-                .Include(r => r.Event)
-                .Include(r => r.User)
-                .AsQueryable();
+            Reviews = await GetLatestReviewDetails();
+        }
 
-            if (!string.IsNullOrEmpty(eventName))
-                query = query.Where(r => r.Event.Title.Contains(eventName));
+        public async Task<List<ReviewDetails>> GetLatestReviewDetails()
+        {
+            var latestReviews = await _db.ReviewsTable
+                .OrderByDescending(r => r.ReviewID)
+                .Take(3)
+                .ToListAsync();
 
-            if (rating.HasValue)
-                query = query.Where(r => r.Rating == rating.Value);
+            var reviewDetails = new List<ReviewDetails>();
 
-            ReviewListing = query.ToList();
+            foreach (var review in latestReviews)
+            {
+                var user = await _db.UsersTable.FindAsync(review.UserID);
+                var eventName = (await _db.EventsTable.FindAsync(review.EventID)).Title;
+
+                var reviewDetail = new ReviewDetails
+                {
+                    ReviewID = review.ReviewID,
+                    UserName = $"{user.Name} {user.Surname}",
+                    EventName = eventName,
+                    Rating = review.Rating,
+                    Comment = review.Comment
+                };
+
+                reviewDetails.Add(reviewDetail);
+            }
+
+            return reviewDetails;
         }
 
         public IActionResult OnPostDelete(int reviewId)
@@ -43,6 +62,15 @@ namespace MetaX.Pages.Admin.Review
                 _db.SaveChanges();
             }
             return RedirectToPage();
+        }
+
+        public class ReviewDetails
+        {
+            public int ReviewID { get; set; }
+            public string UserName { get; set; }
+            public string EventName { get; set; }
+            public int Rating { get; set; }
+            public string Comment { get; set; }
         }
     }
 }
